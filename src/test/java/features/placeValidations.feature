5bfbfdf41f0d7bd1f2f1c8d27415197cffb79557
kp67 <keerthanap773@gmail.com>
2020-07-09 15:27:21 +0530

    first commit

---
+++ src/test/java/features/placeValidations.feature
@@ -0,0 +1,21 @@
+Feature: Validating Place API's
+@AddPlace
+Scenario Outline: Verify if Place is being Succesfully added using AddPlaceAPI
+	Given Add Place Payload with "<name>" "<language>" "<address>"
+	When user calls "AddPlaceAPI" with "Post" http request
+	Then the API call got success with status code 200
+	And "status" in response body is "OK" 
+	And "scope" in response body is "APP"  
+	And verify place_Id created maps to "<name>" using "getPlaceAPI"
+
+Examples:
+	|name		| language |address				|
+	|AAhouse	| English  |World cross center	|
+#	|BBhouse	| Spanish  |Sea cross center	|
+
+@DeletePlace
+Scenario: Verify if Delete Place functionality is working
+	Given Delete Place Payload 
+	When user calls "deletePlaceAPI" with "Post" http request
+	Then the API call got success with status code 200
+	And "status" in response body is "OK"
---
+++ src/test/java/stepDefinations/StepDefination.java
@@ -0,0 +1,99 @@
+package stepDefinations;
+
+import static io.restassured.RestAssured.given;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import resources.APIResources;
+import resources.TestDataBuild;
+import resources.Utils;
+
+public class StepDefination extends Utils{
+	
+	RequestSpecification reqs;
+	ResponseSpecification resp;
+	Response response;
+	TestDataBuild data = new TestDataBuild();
+	JsonPath js;
+	static String place_id;
+	
+		@Given("Add Place Payload with {string} {string} {string}")
+		public void add_place_payload_with(String name, String language, String address) throws IOException {
+					
+			reqs= given().spec(requestSpecification())
+					.body(data.addPlacePayload(name,language,address));
+		}
+
+		@When("user calls {string} with {string} http request")
+		public void user_calls_with_http_request(String resource, String method) {
+		   
+//constructor will be called with value of resource which you pass			
+		APIResources resourceAPI= APIResources.valueOf(resource); //calling a constructor which accepts string
+		System.out.println(resourceAPI.getResource());
+		
+		resp =new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
+		
+		if(method.equalsIgnoreCase("POST"))
+		response= reqs.when().post(resourceAPI.getResource());
+		
+		else if(method.equalsIgnoreCase("GET"))
+			response= reqs.when().get(resourceAPI.getResource());
+		
+		}
+		
+		
+		@Then("the API call got success with status code {int}")
+		public void the_api_call_got_success_with_status_code(Integer int1) {
+		    
+			assertEquals(response.getStatusCode(),200);
+		}
+		
+		@Then("{string} in response body is {string}")
+		public void in_response_body_is(String keyValue, String Expectedvalue) {
+		 
+		
+		assertEquals(getJsonPath(response,keyValue),Expectedvalue);
+		
+		}
+
+
+		@Then("verify place_Id created maps to {string} using {string}")
+		public void verify_place_id_created_maps_to_using(String expectedName, String resource) throws IOException {
+			
+			//request spec
+			place_id =getJsonPath(response,"place_id");
+			reqs= given().spec(requestSpecification()).queryParam("place_id", place_id);
+			user_calls_with_http_request(resource, "GET");
+			String actualName =getJsonPath(response,"name");
+			assertEquals(actualName,expectedName);
+			
+		}
+		
+
+
+		@Given("Delete Place Payload")
+		public void delete_place_payload() throws IOException {
+		reqs = given().spec(requestSpecification()).body(data.deletePlacePayload(place_id));
+		}
+
+
+
+
+
+
+
+}
